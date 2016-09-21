c9f6a479f1c718b747c90037ba466c6c0e911bc0
admin <quanglinh95@gmail.com>
2016-09-21 21:10:23 +0700

    no message

--- Diamonds Game/Diamonds Game/GameScene.swift
+++ Diamonds Game/Diamonds Game/GameScene.swift
@@ -126,6 +126,79 @@ class GameScene: SKScene {
         return false
     }
     
+    //afrer swaping check eat
+    func eatDiamonds() {
+        var count = 0
+        var dau = 0
+        var cuoi = 0
+        //check col
+        for i in 1..<row{
+            for j in 1..<col{
+                if (diamondsArraySTT[i][j] != diamondsArraySTT[i][j-1]){
+                    if (count>=3) {
+                        for k in (j-count)..<j{
+                            diamondsArraySTT[i][k] = -1
+                        }
+                    }
+                    count = 1
+                } else { count += 1 }
+                
+            }
+        }
+        count = 0
+        //check row
+        for j in 1..<col{
+            for i in 1..<row{
+                if (diamondsArraySTT[i][j] != diamondsArraySTT[i][j-1]){
+                    if (count>=3) {
+                        for k in (i-count)..<i{
+                            diamondsArraySTT[k][j] = -1
+                        }
+                    }
+                    count = 1
+                } else { count += 1 }
+            }
+        }
+        count = 0
+        
+        for i in 1..<row{
+            var k = col
+            dau = 1
+            cuoi = 0
+            var xx = 0
+            var yy = 0
+            while (k>1)
+            {
+                if (diamondsArraySTT[i][k] == -1) {
+                    //count += 1
+                    cuoi += 1
+                    Q[cuoi] = locaXY(x: i, y: k)
+                } else{
+                    // count -= 1
+                    if (dau<=cuoi){
+                        swap2Diamonds(i, j1: k, i2: Q[dau].x, j2: Q[dau].y)
+                        // oldPos = locaXY(x: Q[dau].x, y: Q[dau].y-1)
+                        xx = Q[dau].x
+                        yy = Q[dau].y - 1
+                        dau += 1
+                        
+                    } else {
+                        //                        swap2Diamonds(i, j1: k, i2: oldPos.x, j2: oldPos.y)
+                        //                        oldPos = locaXY(x: oldPos.x, y: oldPos.y-1)
+                        swap2Diamonds(i, j1: k, i2: xx, j2: yy)
+                        yy -= 1
+                    }
+                    
+                }
+                k -= 1
+            }
+            
+            
+        }
+        
+        
+    }
+    
     func swap2Diamonds(i1 : Int, j1 : Int ,i2 :Int ,j2:Int) {
         //print("swap: ","{",i1,",",j1,"} ","{",i2,",",j2,"}")
         if ( checkLetSwapOrNot(i1, j1: j1, i2: i2, j2: j2) ){ return }
