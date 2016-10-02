0542a498e760677ca5daaa7d09fbcb698353f3cf
narutomanchester <hiepdz@MacBook-Air.local>
2016-10-02 11:17:36 +0700

    update Tap to

--- Diamonds Game/Diamonds Game/GameOverScene.swift
+++ Diamonds Game/Diamonds Game/GameOverScene.swift
@@ -24,29 +24,29 @@ class GameOverScene: SKScene {
         label.text = message
         label.fontSize = 40
         label.fontColor = SKColor.blackColor()
-        label.position = CGPointMake(self.size.width/2, self.size.height/2)
+        label.position = CGPointMake(self.size.width/2, self.size.height-40)
         self.addChild(label)
         
         //4
-        let replayMessage = "Replay Game"
+        let replayMessage = "Tap to Replay"
         var replayButton = SKLabelNode(fontNamed: "Chalkduster")
         replayButton.text = replayMessage
         replayButton.fontColor = SKColor.blackColor()
-        replayButton.position = CGPointMake(self.size.width/2, 50)
+        replayButton.position = CGPointMake(self.size.width/2, self.size.height / 2)
         replayButton.name = "replay"
         self.addChild(replayButton)
     }
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            let node = self.nodeAtPoint(location) //1
-            if node.name == "replay" { //2
+//            let location = touch.locationInNode(self)
+//            let node = self.nodeAtPoint(location) //1
+//            if node.name == "replay" { //2
                 let reveal : SKTransition = SKTransition.flipHorizontalWithDuration(0.5)
                 let scene = GameScene(size: self.view!.bounds.size)
                 scene.scaleMode = .AspectFill
                 self.view?.presentScene(scene, transition: reveal)
-            }
+ //           }
         }
     }
     
